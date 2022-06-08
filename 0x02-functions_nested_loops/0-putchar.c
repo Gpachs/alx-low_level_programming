15c03b74008f600490b397b54dfe1dd16dd22210
Gpachs <obiomagoldman@gmail.com>
2022-06-08 04:44:19 -0700

    beginner coding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - prints a string "_putchar"
+ * main - Entry point
  *
- * Return: 0
+ * Return: nothing, return void
  */
+
 int main(void)
 
 {
-	int i;
-	char myText[] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-	{
-		putchar(myText[i]);
-	}
-
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-
 }
+
